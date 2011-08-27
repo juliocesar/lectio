ae0b8445b361ec3bee0ee790618e85bec808f0f8
Julio Cesar Ody <julioody@gmail.com>
2011-08-27 17:38:45 +1000

    Able to select articles on the stream and read them

--- public/js/stream.js
+++ public/js/stream.js
@@ -4,17 +4,29 @@
     events    : {
       'click' : 'read'
     },
-    
+        
     render : function() {
       $(this.el).html(this.template(this.model.toJSON()));
       return this;
     },
     
-    read : function() {}
+    read : function() {
+      Lectio.Stream.read(this.model);
+    }
+  });
+  
+  ArticleView = Backbone.View.extend({
+    template : _.template($('#article-template').html()),
+    
+    render : function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    }
   });
   
   Stream = Backbone.View.extend({
-    el : $('#stream'),
+    el      : $('#stream'),
+    reading : $('#reading-now'),
     
     initialize : function() {
       _.bindAll(this, 'add', 'addBunch');
@@ -30,6 +42,21 @@
     
     addBunch : function() {
       Lectio.StreamCollection.each(this.add);
+    },
+    
+    read : function(item) {
+      if (this.reading.attr('data-id') === item.get('id')) return false;
+      var self = this,
+        article = new ArticleView({ model : item });
+      self.reading.removeClass('reading');
+      _.delay(
+        function() {
+          self.reading
+            .html(article.render().el.innerHTML)
+            .attr('data-id', item.get('id'))
+            .addClass('reading');
+        }, 
+      250);
     }
   });
 })(jQuery);
