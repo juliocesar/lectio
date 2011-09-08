9aa857ff5b75599c623470ca9fdad1a671168ce1
Julio Cesar Ody <julioody@gmail.com>
2011-09-08 22:48:02 +1000

    document.ready for app bootup

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,29 @@
 (function($, undefined) {
-  // Collections
-  Lectio.Items = new ItemsCollection;
-  Lectio.ReadLaterCollection = new ReadLaterCollection;
-  Lectio.Items.fetch({
-    success : function() {
-      if ((!location.hash || location.hash === '#/') && Lectio.Items.last()) {
-        Lectio.Stream.read(Lectio.Items.last().get('_id'));
+  $(document).ready(function() {
+    // Collections
+    Lectio.Items = new ItemsCollection;
+    Lectio.ReadLaterCollection = new ReadLaterCollection;
+    Lectio.Items.fetch({
+      success : function() {
+        if ((!location.hash || location.hash === '#/') && Lectio.Items.last()) {
+          Lectio.Stream.read(Lectio.Items.last().get('_id'));
+        }
       }
-    }
+    });
+
+    // Views
+    Lectio.Stream = new Stream;
+    Lectio.MainMenu = new MainMenu;
+    Lectio.ReadLater = new ReadLater;
+    Lectio.ReadLaterMenu = new ReadLaterMenu;
+    Lectio.FocusManager = new FocusManager;
+    Lectio.OfflineManager = new OfflineManager;
+    Lectio.Router = new Router;
+
+    Lectio.FocusManager.bind('focused', function(article) {
+      if (article.is('article.preview')) Lectio.Stream.read(article.attr('id').split('-')[1]);
+    });
+
+    Backbone.history.start();  
   });
-
-  // Views
-  Lectio.Stream = new Stream;
-  Lectio.MainMenu = new MainMenu;
-  Lectio.ReadLater = new ReadLater;
-  Lectio.ReadLaterMenu = new ReadLaterMenu;
-  Lectio.FocusManager = new FocusManager;
-  Lectio.OfflineManager = new OfflineManager;
-  Lectio.Router = new Router;
-
-  Lectio.FocusManager.bind('focused', function(article) {
-    if (article.is('article.preview')) Lectio.Stream.read(article.attr('id').split('-')[1]);
-  });
-
-  Backbone.history.start();
 })(jQuery);
