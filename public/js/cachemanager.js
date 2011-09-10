f38558caeeb0ae4f8e5c971c65173884431f5cf6
David Goodlad <david@goodlad.ca>
2011-09-10 11:05:38 +1000

    Forgot the actual cache manager

---
+++ public/js/cachemanager.js
@@ -0,0 +1,41 @@
+(function($, undefined) {
+  CacheManager = function() {
+    var self = this;
+
+    _.extend(self, Backbone.Events);
+
+    if(!window.applicationCache) return;
+
+    // Check if the browser's managed to fully retrieve an update before we
+    // were called
+    if(window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+      update();
+    }
+
+    $(window.applicationCache)
+      .bind('checking', checking)
+      .bind('updateready', update)
+      .bind('noupdate', noupdate)
+      .bind('error', error)
+      .bind('progress', progress);
+
+    function error(e) { console.log('error', e); }
+    function checking() {
+      console.log("Checking for update");
+    }
+
+    function noupdate() {
+      console.log("No updates available");
+    }
+
+    function update() {
+      console.log("New version available");
+      window.applicationCache.swapCache();
+      //window.location.reload();
+    }
+
+    function progress(event) {
+      console.log("Got cache progress", event);
+    }
+  };
+})(jQuery);
